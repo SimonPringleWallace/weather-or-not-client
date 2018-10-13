0e3db0929887524a30dde43830912e19a624b0e0
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-10-13 12:22:42 -0400

    Add Card Component to Location Card
    - Install Ant npm
    - Add ant card component
    - Add minor styling

--- src/homepage/locationcard.js
+++ src/homepage/locationcard.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {destroyLocation} from './homepage_api.js'
+import { DatePicker, Card, Icon, Avatar } from 'antd'
 import './locationcard.scss'
+import 'antd/dist/antd.css'
+const { Meta } = Card
 
 class LocationCard extends React.Component {
   destroyLocation(id) {
@@ -12,9 +15,23 @@ class LocationCard extends React.Component {
 
   render () {
     return (
-      <div className='location-card-flex'>
+      <Card
+        hoverable
+        style={{ width: 300}}
+        bodyStyle={{width: 300}}
+        cover={<img alt="example" src={require('../header/weather-or-not-logo.png')} />}
+        actions={[<Icon key={this.props.id} type="setting" />, <Icon key={this.props.id + 1} type="edit" />, <Icon key={this.props.id + 2} type="ellipsis" />]}
+      >
+        <Meta
+          avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+          title={`${this.props.city}, ${this.props.state}`}
+          description="This is the description"
+        />
+      </Card>
+      /* <div className='location-card-flex'>
         <a className='getForecast' onClick={this.getLocationForecast.bind(this, this.props.city)}>
           <div>
+            <DatePicker>pick dates</DatePicker>
             <p>{this.props.city}</p>
             <p>{this.props.usState}</p>
             <p>{this.props.longitude}</p>
@@ -23,7 +40,7 @@ class LocationCard extends React.Component {
           </div>
         </a>
         <button value={this.props.id} onClick={this.destroyLocation.bind(this, this.props.id)}>Destroy!!!</button>
-      </div>
+      </div> */
     )
   }
 }
